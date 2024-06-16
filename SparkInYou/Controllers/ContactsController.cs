5b1fc63cf96d89f5d4ac163cbbc7a4174fbbb6ae
Oyasumi <95757737+FLAWWWWW@users.noreply.github.com>
2024-06-16 10:20:51 +0500

    Добавление страницы Contacts с возможностью отправки формы

---
+++ SparkInYou/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SparkInYou.Models;
+
+namespace SparkInYou.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact) // contacts/check
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ SparkInYou/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SparkInYou.Models
+{
+    public class Contact
+    {
+        // Данные, которые мы получаем от пользователя в форме
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Вам нужно ввести почту")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [StringLength(150, ErrorMessage = "Текст менее 150 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
+        public string Message { get; set; }
+    }
+}
