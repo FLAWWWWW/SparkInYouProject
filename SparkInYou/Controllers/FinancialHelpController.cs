9ccac063e08d08474246eacdc66d100a508da431
Oyasumi <95757737+FLAWWWWW@users.noreply.github.com>
2024-06-20 14:53:35 +0500

    Добавлена страница "Финансовая помощь" и доделана Главная страница

---
+++ SparkInYou/Controllers/FinancialHelpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SparkInYou.Controllers
+{
+    public class FinancialHelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
