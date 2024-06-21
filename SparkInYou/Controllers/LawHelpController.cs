69be6719f4f87c0d997334956e1c8f540f553eae
Oyasumi <95757737+FLAWWWWW@users.noreply.github.com>
2024-06-22 00:25:54 +0500

    Добавлены вкладки "Социальная Помощь", "Юридическая Помощь","Личный Кабинет"

---
+++ SparkInYou/Controllers/LawHelpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SparkInYou.Controllers
+{
+    public class LawHelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
